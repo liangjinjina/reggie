4d075643c2681fbd26422adff65b7799135007c9
fanshuais <1357212355@qq.com>
2022-05-23 16:19:45 +0800

    2022/5/23 2

---
+++ src/main/java/com/itwj/reggie/service/CategoryService.java
@@ -0,0 +1,7 @@
+package com.itwj.reggie.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.itwj.reggie.entity.Category;
+
+public interface CategoryService extends IService<Category> {
+}
